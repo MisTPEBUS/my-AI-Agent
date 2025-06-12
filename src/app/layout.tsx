a944e577a0ea3c2bd00680fc9720d4c920f65beb
Lobinda <lobinda123@gmail.com>
2025-06-12 11:31:16 +0800

    feat:google search setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,8 +15,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "AI智能平台",
-  description: "AI智能平台",
+  title: "路邊攤的AI智能平台",
+  description: "路邊攤的AI智能平台",
 };
 
 export default function RootLayout({
@@ -25,6 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="google-site-verification=y_2cCG0dzhqsNZjDfBrs1ZG_BE2-iH_jhi2rD3A30p0"
+        />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
